1c169331794a48319e4e98cf2dfefb167223d0a5
Austin Henriksen <austin.r.henriksen79@gmail.com>
2023-05-02 12:39:10 -0400

    Implemented encryption and verification logic. Decryption is up next.

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,16 +1,11 @@
 
 use crate::content_summarizer::ContentSummarizer;
-use crate::{constants, signature_utils};
+use crate::io_utils::{read_to_buffer, ReadResult};
+use crate::{constants::*, signature_utils};
 use std::fs::File;
-use std::io::{ErrorKind, Result, Seek, SeekFrom, Write};
+use std::io::{Error, ErrorKind, Result, Seek, SeekFrom, Write};
 use std::path::Path;
 use ed25519_dalek::Keypair;
-
-
-
-
-
-
 
 pub struct PlainFileReader {
     file: File,
@@ -21,15 +16,11 @@ impl PlainFileReader {
         let file = File::open(path)?;
         Ok(Self { file })
     }
+
+    pub fn try_read_from(&mut self, buffer: &mut [u8]) -> ReadResult {
+        read_to_buffer(&mut self.file, buffer)
+    }
 }
-
-
-
-
-
-
-
-
 
 pub struct CipherFileWriter {
     file: File,
@@ -39,9 +30,10 @@ impl CipherFileWriter {
     pub fn create_new<P: AsRef<Path>>(path: P) -> Result<Self> {
         let mut file = File::create_new(path)?;
 
-        let offset = file.seek(SeekFrom::Start(constants::FILE_HEADER_SIZE))?;
-        if offset != constants::FILE_HEADER_SIZE {
-            return Err(ErrorKind::Interrupted.into())
+        let offset = file.seek(SeekFrom::Start(FILE_HEADER_SIZE))?;
+        if offset != FILE_HEADER_SIZE {
+            let message =  format!("Failed to seek to content start! Expected to be at '{FILE_HEADER_SIZE}', but reached '{offset}'.");
+            return Err(Error::new(ErrorKind::Other, message));
         }
 
         Ok(Self { file })
@@ -61,23 +53,25 @@ impl CipherFileWriter {
         let (hash_value, content_length) = content_summarizer.finalize(initial_counter_value, encryption_key_salt);
         let signed_hash_value = signature_utils::sign_message(&hash_value, signing_keys);
 
-        self.file.write_all(&constants::FILE_END)?;
+        self.file.write_all(&FILE_END)?;
 
         let start_offset = self.file.seek(SeekFrom::Start(0))?;
         if start_offset != 0 {
-            return Err(ErrorKind::Interrupted.into())
+            let message =  format!("Failed to seek to file start! Expected to be at '0', but reached '{start_offset}'.");
+            return Err(Error::new(ErrorKind::Other, message));
         }
 
-        self.file.write_all(&constants::FILE_BEG)?;
+        self.file.write_all(&FILE_BEG)?;
         self.file.write_all(signing_keys.public.as_bytes())?;
         self.file.write_all(&signed_hash_value.to_bytes())?;
         self.file.write_all(&initial_counter_value.to_be_bytes())?;
         self.file.write_all(&encryption_key_salt.to_be_bytes())?;
         self.file.write_all(&content_length.to_be_bytes())?;
 
-        let header_offset = self.file.seek(SeekFrom::Start(0))?;
-        if header_offset != constants::FILE_HEADER_SIZE {
-            return Err(ErrorKind::Interrupted.into())
+        let header_offset = self.file.stream_position()?;
+        if header_offset != FILE_HEADER_SIZE {
+            let message =  format!("Failed to write header! Expected to be at '{FILE_HEADER_SIZE}', but reached '{header_offset}'.");
+            return Err(Error::new(ErrorKind::Other, message));
         }
 
         self.file.sync_all()
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,32 @@
 #![feature(file_create_new)]
 #![feature(slice_as_chunks)]
 
+// So, we want 3 things:
+// 1) Take a file, and convert it to an ARH file                IMPLEMENTED
+// 2) Take an ARH file and convert it to a file
+// 3) Be able to open and randomly access an ARH file
+
+// ===== TODO ===== //
+// Files that are still in progress: main, encryption, decryption, content_summarizer.
+// Add a license, README, cleanup the gitignore, and modify Cargo config file.
+// Add some kind of progress callback to the main functions, so they can report how many bytes they've processed.
+//
+// See if using a ping-pong buffer for reading-processing input data improves performance.
+// Add module comments to all the files.
+// Maybe we should test `ReadResult::Error`?
+// Should we be zeroing other things?
+
+// Should we be encrypting the length of the file?
+// That's the only thing we aren't encrypting that has the potential to be encrypted.
+
+// Instead of taking files, maybe the encryption/decryption should just take Readers/Writers so we can have in-memory tests.
+
+// TODO: go through and improve all the error messages to actually report useful information.
+
+// TODO add a Read/Write wrapper for interacting with files programmatically that allows seeking and such.
+
+// TODO add user interactivity (that can be undone with a command line flag).
+
 pub mod decryption;
 pub mod encryption;
 mod constants;
@@ -12,138 +38,202 @@ mod key_gen_utils;
 mod signature_utils;
 mod stream_cipher;
 
-use std::fs::File;
-use std::io::{Read, Seek, SeekFrom};
-use std::path::PathBuf;
-use rand::Rng;
-
+use crate::content_summarizer::ContentSummarizer;
+use crate::derived_key_data::DerivedKeyData;
+use crate::encryption::{CipherFileWriter, PlainFileReader};
+use crate::stream_cipher::StreamCipher;
+use std::collections::HashSet;
+use std::io::{Error, ErrorKind, Result};
+use std::path::{Path, PathBuf};
 use aes::Aes256;
 use cipher::Key;
+use decryption::CipherFileReader;
+use ed25519_dalek::{Keypair, PublicKey, Signature};
+use io_utils::ReadResult;
+use rand::Rng;
+use rayon::prelude::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
+use zeroize::Zeroize;
 
-// TODO add a license, README, cleanup the gitignore, and modify Cargo config file.
+/// TODO
+pub fn verify_file(
+    input_path: &(impl AsRef<Path> + ?Sized),
+    trusted_public_keys: &HashSet<[u8; 32]>,
+    expect_eof: bool,
+) -> Result<()> {
+    // Open the file we're going to verify.
+    let mut input_file = CipherFileReader::open(input_path)?;
 
-// TODO add module comments to things.
-// TODO maybe zeroize other things?
+    // Create a summarizer to digest the inputted cipher text.
+    let mut summarizer = ContentSummarizer::default();
 
-// TODO should we test `ReadResult::Error`?
+    // Allocate a working buffer and counter.
+    let mut buffer = [0_u8; 16384];
+    let mut counter = 0;
+    // Loop through the data in the input file.
+    let expected_content_length = input_file.header().content_length;
+    while counter < expected_content_length {
+        let read_length = std::cmp::min((expected_content_length - counter) as usize, buffer.len());
+        let read_buffer = &mut buffer[0..read_length];
 
-// ===========================================================================================
-// Okay, I think that we have literally everything we need now to implement the fils structure.
-// All that's left is to write the IO side of things, some comments, and some tests!
-// ===========================================================================================
-//Come back, and finish writing the comments on everything that has a TODO
-//Then we need to think of and write a system for encrypting a file into a new file.
-//
-//That should be portable to make it easy to write a file too.
-//We'll worry about randomly accessing a file later on!
-//
-//Ooooh, maybe we have some kind of report metrics that can be run while it's going!
-//That's actually a pretty cool idea and we should totally add that! Like, some running counter?
-//
-//1) Text people back
-//
-//2) Sand the poly board flatter
-//3) Sand the poly shelves smoother
-//4) Spray the next layer onto the poly shelves
-//5) Paint the next layer onto the poly board
-//6) Start painting the first layer onto the poly shield
+        match input_file.try_read_from(read_buffer) {
+            // TODO
+            ReadResult::Full => {
+                summarizer.update(read_buffer);
+                counter += read_length as u64;
+            }
 
+            // TODO
+            ReadResult::Eos(length) => {
+                counter += length as u64;
+                let message = format!("File header specified it contained {expected_content_length} bytes, but EOF was encountered after reading only {counter} bytes.");
+                return Err(Error::new(ErrorKind::UnexpectedEof, message));
+            }
 
-
-
-// So, we read 4096 bytes from a file into a buffer,
-// Then we break it up and encrypt it
-// Then we run it through the summarizer
-// Then we write it to our output file
-// We continue doing that until we've reached the end of the file.
-// then, we write the end marker, and seek to the beginning of the file to write in the header data.
-// Yeah, we don't even need a ping-pong buffer!!
-
-
-
-
-
-// TODO: open a file, and implement a ping-pong buffer, then run stuff over the buffers and into 
-
-
-// So, now we get to work on my least favorite part... File IO stuff. Hooray.
-
-
-// So, we want 3 things:
-// 1) Take a file, and convert it to an ARH file
-// 2) Take an ARH file and convert it to a file
-// 3) Be able to open and randomly access an ARH file
-
-pub fn encrypt_file(
-    file_path: &str,
-    master_encryption_key: &Key<Aes256>,
-    signing_keys: ed25519_dalek::Keypair,
-) {
-    // Open the file we're going to encrypt.
-    let Ok(mut input) = File::open(file_path) else { panic!("failed to open input"); };
-
-    // Create a file to write the output into. It's the same as the input file, but with an extension of 'arh' on it.
-    let mut output_path = PathBuf::from(file_path.to_owned() + ".arh");
-    let Ok(mut output) = File::create(output_path) else { panic!("failed to open output"); };
-
-    // Reserve space at the beginning of the file and seek to the contents portion of the file.
-    let Ok(offset) = output.seek(SeekFrom::Start(constants::FILE_HEADER_SIZE)) else { panic!("failed to seek contents"); };
-    if offset != constants::FILE_HEADER_SIZE { panic!("seek only went to: {offset}"); };
-
-
-
-    // First, we derive a key that is specific to the provided data.
-    // This ensures that even if an attacker manages to compromise this data or determine the key used to encrypt it,
-    // all other data encrypted with the provided master key remains safe.
-    //
-    // derive a new file-specific key from the master encryption key, and initialize an AES engine with it.
-    let derived_key_data::DerivedKeyData { derived_key, key_salt } = derived_key_data::DerivedKeyData::derive_new_from(master_encryption_key);
-    let cipher = stream_cipher::StreamCipher::initialize(&derived_key.into());
-
-    // Randomly generate a 128bit IV to initialize the AES-CTR engine with.
-    let initial_value: u128 = rand::thread_rng().gen();
-
-    // Create a summarizer for storing the length and hash of the input data.
-    let mut summarizer = content_summarizer::ContentSummarizer::default();
-    
-
-
-    // Allocate a buffer we can read input data into.
-    let mut buffer = [0_u8; 4096];
-
-    // Read data from the input, encrypt it, and write it to the output.
-
-
-
-    loop {
-        // Try to read 4096 bytes from the file into the buffer.
-        match input.read(&mut buffer) {
-            Ok(len) => if len != 4096 { panic!("Only read {len} bytes!") }
-            Err(err) => panic!("failed to read: {err:?}"),
+            // If we encounter an unrecoverable error while reading, return it immediately.
+            ReadResult::Error(error) => return Err(error),
         }
     }
-}
+    debug_assert_eq!(counter, expected_content_length);
 
-/*
-impl Aes256StreamCipher {
-    pub fn encrypt_data(&self, data: &mut [u8], iv: u128) {
-        let (blocks, remainder) = data.as_chunks_mut::<16>();
+    // Ensure that the file ends with a `FILE_END` marker, and that EOF is immediately after if `expect_eof` is true.
+    input_file.check_file_end(expect_eof)?;
 
-        blocks.into_par_iter().enumerate().for_each(|(i, block)| {
-        })
+    // Compute a hash of the file's content.
+    let (hash_value, content_length) = summarizer.finalize(
+        input_file.header().initial_counter_value,
+        input_file.header().encryption_key_salt,
+    );
+    debug_assert_eq!(content_length, expected_content_length);
+
+    // TODO
+    let public_signing_key = PublicKey::from_bytes(&input_file.header().signature_key)
+        .map_err(|error| Error::new(ErrorKind::Other, error))?;
+    if !trusted_public_keys.contains(public_signing_key.as_bytes()) {
+        return Err(Error::new(ErrorKind::Other, "File is signed with an untrusted key")); // TODO better message and functionality
     }
-}
-*/
 
-pub fn encrypt_data<const L: usize>(
-    data: &mut [u8],
-    master_encryption_key: &[u8; 32],
-) {
-  //let cipher = Aes256StreamCipher::initialize(&derived_key);
-  //cipher.encrypt_data(data, initial_value);
-    
+    // TODO
+    let signature = Signature::from_bytes(&input_file.header().signature)
+        .map_err(|error| Error::new(ErrorKind::Other, error))?;
+    if !signature_utils::verify_signature(&signature, &public_signing_key, &hash_value) {
+        return Err(Error::new(ErrorKind::Other, "File signature does not match contents! File has been altered or corrupted.")) // TODO better message
+    }
+
+    Ok(())
 }
+
+/// TODO
+pub fn encrypt_file(
+    input_path: &(impl AsRef<Path> + ?Sized),
+    master_encryption_key: &Key<Aes256>,
+    signing_keys: &Keypair,
+) -> Result<()> {
+    // Open the file we're going to encrypt.
+    let mut input_file = PlainFileReader::open(input_path)?;
+
+    // Create a file to write the output into. It's the same as the input, but with ".arh" appended to it.
+    let mut output_file_path = input_path.as_ref().to_owned();
+    output_file_path.set_extension(
+        input_path.as_ref()
+            .extension()
+            .map_or(
+                PathBuf::from("arh"),
+                |extension| {
+                    let mut new_extension = extension.to_owned();
+                    new_extension.push(".arh");
+                    PathBuf::from(new_extension)
+                },
+            )
+    );
+    let mut output_file = CipherFileWriter::create_new(output_file_path)?;
+
+    // Create an encryption key specific to this file, derived from the provided master key.
+    // This ensures that even if an attacker manages to compromise this data or determine the key used to encrypt it,
+    // all other data encrypted with the provided master key remains safe.
+    let DerivedKeyData { mut derived_key, key_salt } = DerivedKeyData::derive_new_from(master_encryption_key);
+    // Initialize a stream cipher with the derived key, then attempt to destroy it in memory.
+    let cipher = StreamCipher::initialize(&derived_key);
+    derived_key.zeroize();
+
+    // Create a summarizer to digest the outputted cipher text.
+    let mut summarizer = ContentSummarizer::default();
+
+    // Randomly generate a 128bit value to start the AES-CTR counter at.
+    let initial_value: u128 = rand::thread_rng().gen();
+
+    // Allocate a working buffer and counter.
+    let mut buffer = [0_u8; 16384];
+    let mut counter = initial_value;
+    // Loop through the data in the input file.
+    loop {
+        match input_file.try_read_from(&mut buffer) {
+            // TODO
+            ReadResult::Full => {
+                let blocks = unsafe { buffer.as_chunks_unchecked_mut::<16>() };
+                let block_counter_pairs = blocks.into_par_iter()
+                    .enumerate()
+                    .map(|(index, block)| (index as u128 + counter, block));
+
+                cipher.process_blocks(block_counter_pairs);
+
+                summarizer.update(&buffer);
+                output_file.write_content(&buffer)?;
+
+                counter += buffer.len() as u128;
+            }
+
+            // TODO
+            ReadResult::Eos(length) => {
+                let filled_buffer = &mut buffer[0..length];
+
+                let (blocks, remainder) = filled_buffer.as_chunks_mut::<16>();
+                let block_counter_pairs = blocks.into_par_iter()
+                    .enumerate()
+                    .map(|(index, block)| (index as u128 + counter, block));
+
+                cipher.process_blocks(block_counter_pairs);
+                cipher.process_block(blocks.len() as u128 + counter, remainder);
+
+                summarizer.update(filled_buffer);
+                output_file.write_content(filled_buffer)?;
+
+                counter += length as u128;
+                break;
+            }
+
+            // If we encounter an unrecoverable error while reading, return it immediately.
+            ReadResult::Error(error) => return Err(error),
+        }
+    }
+
+    // We've finished writing the contents of the encrypted output file.
+    // Before returning, we finalize the file by writing the ARH wrapper around the contents.
+    output_file.finalize(signing_keys, summarizer, initial_value, key_salt)
+}
+
+
 
 fn main() {
-    println!("Hello, world!");
+    let signing_keys = key_gen_utils::generate_new_signing_keys();
+    let master_encryption_key = key_gen_utils::generate_new_encryption_key();
+    match encrypt_file("/Users/austin/archive-fs/test.txt", &master_encryption_key, &signing_keys) {
+        Ok(_) => println!("Finished encrypting Successfully!"),
+        Err(err) => {
+            println!("Writing Error: {err:?}");
+            return;
+        }
+    }
+
+    let mut trusted_public_keys = HashSet::new();
+    trusted_public_keys.insert(signing_keys.public.to_bytes());
+
+    match verify_file("/Users/austin/archive-fs/test.txt.arh", &trusted_public_keys, true) {
+        Ok(_) => println!("Finished verifying Successfully!"),
+        Err(err) => {
+            println!("Verification Error: {err:?}");
+            return;
+        }
+    }
+
+    println!("COMPLETED!");
 }
